b08c2540828d62170911fccfdbbcb8f7d01c174f
ewhycode <152671909+ewhycode@users.noreply.github.com>
2023-12-21 12:10:29 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let addToButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDo.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+    })
+})
